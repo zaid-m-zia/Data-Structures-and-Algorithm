6fe758e941acbb1d8725f329e4da6ae90971127f
Zaid Zia <zaidzia900@gmail.com>
2026-06-23 21:52:07 +0530

    finished revising java now on to builing problem solving skills

--- Basic/Basic.java
+++ Basic/Basic.java
@@ -1,6 +1,23 @@
+//q- take input of size and the array inputs and print the sum of all the numbers in the array
+
+import java.util.Scanner;
 public class Basic {
     public static void main(String[] args) {
-        System.out.println("this is the cat");
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+        //storing the taken input in array
+        for(int i=0;i<=size-1;i++){
+            arr[i] = scanner.nextInt();
+        }
+
+        //printing the array made
+        int sum = 0;
+        for(int j=0;j<=size-1;j++){
+            sum += arr[j];
+        }
+        System.out.println(sum);
+        scanner.close();
     }
 
 }
