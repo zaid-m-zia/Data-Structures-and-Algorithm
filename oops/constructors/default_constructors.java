62253d6c5b53371fa0cebcf4b1f351e8f5798ffc
Zaid Zia <zaidzia900@gmail.com>
2026-06-30 01:44:59 +0530

    done with constructors

---
+++ oops/constructors/default_constructors.java
@@ -0,0 +1,12 @@
+
+
+public class default_constructors {
+    default_constructors(){
+        System.out.println("hello");
+        System.out.println("hello this is statement called by object");
+    }
+    public static void main(String[] args) {
+        default_constructors obj = new default_constructors();
+
+    }
+}
--- oops/private_access_specifiers/pvt.java
+++ oops/private_access_specifiers/pvt.java
@@ -1,3 +1,8 @@
 public class pvt {
-    
+    private static void Private(){
+        System.out.println("private print is called");
+    }
+    public static void main(String[] args) {
+        Private();
+    }
 }
