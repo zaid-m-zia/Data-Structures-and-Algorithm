91fdfcfbdad15f403808119766f7bc67dd1b369c
Zaid Zia <zaidzia900@gmail.com>
2026-07-08 00:24:41 +0530

    starting with hashing

---
+++ Hashing/basic.java
@@ -0,0 +1,11 @@
+package TUF.Hashing;
+
+public class basic {
+    public static void main(String[] args) {
+        // assigning unique values understanding keys and what are they.
+       String a = "hello world";
+       System.out.println(a.hashCode());
+       Integer b = 4;
+       System.out.println(b.hashCode());
+    }
+}
